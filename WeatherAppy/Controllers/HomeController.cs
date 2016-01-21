a79e3b8ce1d2691e564893f492b7924c41f0de43
fr0ntera <greasby.a@gmail.com>
2016-01-20 20:57:30 -0500

    setting up

---
+++ WeatherAppy/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WeatherAppy.Models;
+
+namespace WeatherAppy.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: WeatherJS
+        public ActionResult WeatherJS()
+        {
+            return View();
+        }
+
+        //GET:  WeatherOrNot
+        public ActionResult WeatherOrNot()
+        {
+            return View();
+        }
+
+        //GET:  GetWeather
+        public JsonResult GetWeather()
+        {
+            WeatherClass weath = new WeatherClass();
+            return Json(weath.getWeatherForecast(), JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ WeatherAppy/Models/WeatherClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+
+namespace WeatherAppy.Models
+{
+    class WeatherClass
+    {
+        public Object getWeatherForecast()
+        {
+            string appid = "74794995f6171d13d4ff10d4f1b5a9f5";
+            string url = "http://api.openweathermap.org/data/2.5/weather?q=Cleveland&APPID=74794995f6171d13d4ff10d4f1b5a9f5&units=imperial";
+            //synchronous client
+        var client = new WebClient();
+        var content = client.DownloadString(url);
+        var serializer = new JavaScriptSerializer();
+        var jsonContent = serializer.Deserialize<Object>(content);
+        return jsonContent;
+        }
+    }
+}
